dfe995681bb9e00e36589b73457cefec960b4460
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-12-20 02:29:26 +0100

    style: Change grid layout to flex

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import styles from "./Hero.module.css";
-import heroBg from "../assets/fondo-home.jpg";
+import heroBg from "../assets/possible_bg2.png";
 import Header from "./Header";
 
 export default function Hero() {
@@ -16,8 +16,8 @@ export default function Hero() {
                 <p className={styles.subHeadline}>
                     Bloomji es un dispositivo diseñado para ayudarte a cuidar de tu planta, convirtiéndola en una mascota que interactúa contigo mediante emociones.
                 </p>
+                <p>¡Inscríbete a la waitlist!</p>
                 <div className={styles.emailCapture}>
-                    <p>¡Inscríbete a la waitlist!</p>
                     <input type="email" name="email" className={styles.input} placeholder="Tu e-mail" />
                     <button className={styles.ctaButton}>
                         Apúntate ahora
